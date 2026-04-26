a7a8e9957b0a0db022724dc454b013af6e1bf634
enzomtp <enzojore@live.fr>
2026-04-26 13:30:49 +0200

    fix(cors): correct regex for allowed origins in CORS middleware

--- src/app.py
+++ src/app.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"^https?://(localhost|127\.0\.0\.1)(:\\d+)?$",
+    allow_origin_regex=r"^https?://(localhost|127\.0\.0\.1)(:\d+)?$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
